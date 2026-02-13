8295691554783c0bbe6aae117de7d05bbdf7c69e
Mrtindanzor <ktindanzor@gmail.com>
2026-02-13 11:35:42 +0000

    Added metadata to pages

--- src/app/vote/[pollId]/page.tsx
+++ src/app/vote/[pollId]/page.tsx
@@ -3,7 +3,9 @@ import {
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
+import { cache } from "react";
 import { fetchPollVote, POLL_KEY } from "@/domain/poll";
 import { PollVotePage } from "@/features/poll-vote";
 import { tryCatch } from "@/shared/utils/tryCatch";
@@ -14,11 +16,29 @@ type VotePageProps = {
   params: Promise<{ pollId: string }>;
 };
 
+const fetchPoll = cache(async (pollId: string) => {
+  const [poll] = await tryCatch(fetchPollVote(pollId));
+  return poll;
+});
+
+export async function generateMetadata({
+  params,
+}: VotePageProps): Promise<Metadata> {
+  const { pollId } = await params;
+  const poll = await fetchPoll(pollId);
+
+  if (!poll) return { title: "Consensus - PollMaker" };
+
+  return {
+    title: `Consensus - ${poll.title}`,
+  };
+}
+
 const qc = new QueryClient();
 
 export default async function Page({ params }: VotePageProps) {
   const { pollId } = await params;
-  const [poll] = await tryCatch(fetchPollVote(pollId));
+  const poll = await fetchPoll(pollId);
 
   if (!poll) return notFound();
 
