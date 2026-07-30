46083e25e3bbe81e2747de72c0f45733875cca39
Mrtindanzor <ktindanzor@gmail.com>
2026-07-30 16:49:26 +0000

    Fixed import client hooks in server files

--- src/features/poll/polls.useVote.ts
+++ src/features/poll/polls.useVote.ts
@@ -1,3 +1,5 @@
+"use client";
+
 import { useQuery } from "@tanstack/react-query";
 import { useCallback, useLayoutEffect, useState } from "react";
 import { type PollType, pollQuery } from "@/features/poll";
