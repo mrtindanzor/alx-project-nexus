2477b638ca77a62c3dc8a01696db8392a156e2d3
Mrtindanzor <ktindanzor@gmail.com>
2026-07-30 16:38:26 +0000

    Added env sample

--- src/config/publicUrls.ts
+++ src/config/publicUrls.ts
@@ -1,9 +1,7 @@
 import { getEnv } from "./utils/getEnv";
 
-const appUrl = getEnv({ name: "NEXT_PUBLIC_APP_URL" });
 const serverUri = getEnv({ name: "NEXT_PUBLIC_SERVER_URI" });
 
 export const publicUrls = {
-  appUrl,
   serverUri,
 };
